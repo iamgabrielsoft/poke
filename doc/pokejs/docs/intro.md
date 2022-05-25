06501dec2bd5a6b89d0718a8ed8606515a76a5ad
iamgabrielsoft <iamgabrielsoft@gmail.com>
2022-05-25 23:45:43 +0100

    created a simple markdown file

---
+++ doc/pokejs/docs/intro.md
@@ -0,0 +1,45 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Pokejs
+
+Let's write Promise base  **request better with pokejs**.
+
+## What is Pokejs?
+
+👈🏽 A Super handy HTTP Client tool written in written for node. On the serverside, its uses node built-in http module for sending and receivin request.
+
+
+### Features
+
+- Support Promise base API
+- Intercept request and response
+- Cancel requests
+- Automatic transforms for JSON data
+- Use Webhooks
+
+## Installing
+
+
+using npm  
+
+```bash
+npm install poke-js
+```
+
+using yarn
+
+```bash
+yarn add poke-js
+```
+
+
+using bower
+
+```bash
+bower install poke-js
+```
+
+More Features coming up...
---
+++ doc/pokejs/docusaurus.config.js
@@ -0,0 +1,117 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+  
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Pokejs',
+  tagline: '👈🏽 A Super handy HTTP Client written in TypeScript. ',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'pokejs', // Usually your GitHub org/user name.
+  projectName: 'poke', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/LawsonCheng/poke',
+        },
+
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Pokejs',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/LawsonCheng/poke',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Introduction',
+                to: '/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/iamgabrielsoft',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/LawsonCheng/poke',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Pokejs. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
